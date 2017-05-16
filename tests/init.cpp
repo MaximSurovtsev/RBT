38c58c902858cdaad93dd13244f37eeb6e45c243
MaximSurovtsev <gr4m0zeka@gmail.com>
2017-05-16 09:22:31 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -50,6 +50,54 @@ SCENARIO("insert3", "[init]")
   REQUIRE(tree.search(5) != 0);
 }
 
+SCENARIO ("insert4", "[init]")
+{
+  RedBlackTree<int> tree;
+  tree.insert(3);
+  tree.insert(1);
+  tree.insert(2);
+  REQUIRE(tree._root() == tree.search(2));
+  REQUIRE(tree._color(2) == 1);
+  REQUIRE(tree._color(1) == 0);
+  REQUIRE(tree._color(3) == 0);
+  REQUIRE(tree.search(1) != 0);
+  REQUIRE(tree.search(2) != 0);
+  REQUIRE(tree.search(3) != 0);
+}
+
+SCENARIO("insert5", "[init]")
+{
+  RedBlackTree<int> tree;
+  tree.insert(3);
+  tree.insert(2);
+  tree.insert(1);
+  REQUIRE(tree._root() == tree.search(2));
+  REQUIRE(tree._color(2) == 1);
+  REQUIRE(tree._color(1) == 0);
+  REQUIRE(tree._color(3) == 0);
+  REQUIRE(tree.search(1) != 0);
+  REQUIRE(tree.search(2) != 0);
+  REQUIRE(tree.search(3) != 0);
+}
+
+SCENARIO("insert6", "[init]")
+{
+  RedBlackTree<int> tree;
+  tree.insert(5);
+  tree.insert(7);
+  tree.insert(6);
+  REQUIRE(tree._root() == tree.search(6));
+  REQUIRE(tree._color(6) == 1);
+  REQUIRE(tree._color(5) == 0);
+  REQUIRE(tree._color(7) == 0);
+  REQUIRE(tree.search(5) != 0);
+  REQUIRE(tree.search(6) != 0);
+  REQUIRE(tree.search(7) != 0);
+}
+
+
+
+
 
 
 
